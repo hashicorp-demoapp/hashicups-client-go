a172d9c52366dc9cbaca9d6d5b8926a92da00f9f
sylviamoss <moss@hashicorp.com>
2021-03-15 17:09:17 +0100

    remove CreateCustomOrder

--- orders.go
+++ orders.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
-	"strconv"
 	"strings"
 )
 
@@ -33,98 +32,6 @@ func (c *Client) GetOrder(orderID string) (*Order, error) {
 // CreateOrder - Create new order
 func (c *Client) CreateOrder(orderItems []OrderItem) (*Order, error) {
 	rb, err := json.Marshal(orderItems)
-	if err != nil {
-		return nil, err
-	}
-
-	req, err := http.NewRequest("POST", fmt.Sprintf("%s/orders", c.HostURL), strings.NewReader(string(rb)))
-	if err != nil {
-		return nil, err
-	}
-
-	body, err := c.doRequest(req)
-	if err != nil {
-		return nil, err
-	}
-
-	order := Order{}
-	err = json.Unmarshal(body, &order)
-	if err != nil {
-		return nil, err
-	}
-
-	return &order, nil
-}
-
-// CreateCustomOrder - Create new custom order
-// A custom order is when a coffee ingredient quantity is different from what is pre-defined.
-//
-// For example:
-// By default, the 'Vaulatte' ingredients are 40ml of Espresso and 300ml Semi Skimmed Milk.
-// A custom order is when the Vaulatte is customised to get 50ml of Espresso instead.
-//
-// The order at the end will have a new custom coffee type. The coffee will be a new version of the
-// Vaulatte coffee with 50ml of Espresso and 300ml Semi Skimmed Milk.
-func (c *Client) CreateCustomOrder(orderItems []OrderItem) (*Order, error) {
-	coffees, err := c.GetCoffees()
-	if err != nil {
-		return nil, err
-	}
-
-	ois := []OrderItem{}
-	for _, o := range orderItems {
-		order := &o
-		customIngredients := order.Coffee.Ingredient
-
-		realIngredients, err := c.GetCoffeeIngredients(strconv.Itoa(order.Coffee.ID))
-		if err != nil {
-			return nil, err
-		}
-
-		// Update order with custom coffee
-		for _, coffee := range coffees {
-			if coffee.ID == order.Coffee.ID {
-				if order.Coffee.Name == "" {
-					return nil, fmt.Errorf("Coffee %d must have a name", order.Coffee.ID)
-				}
-				if order.Coffee.Name == coffee.Name {
-					return nil, fmt.Errorf("Coffee %s must have a different name from the original coffee", order.Coffee.Name)
-				}
-				coffee.Name = order.Coffee.Name
-				newCoffe, err := c.CreateCoffee(coffee)
-				if err != nil {
-					return nil, err
-				}
-				coffee.ID = newCoffe.ID
-				order.Coffee = coffee
-				continue
-			}
-		}
-
-		// Add ingredients to the custom coffee
-		for _, ingredient := range realIngredients {
-			for _, customIngredient := range customIngredients {
-				if ingredient.ID == customIngredient.ID {
-					// Update ingredient quantity according to customisation
-					ingredient.Quantity = customIngredient.Quantity
-					continue
-				}
-			}
-			_, err := c.CreateCoffeeIngredient(order.Coffee, ingredient)
-			if err != nil {
-				return nil, err
-			}
-		}
-
-		ois = append(ois, OrderItem{
-			Coffee: Coffee{
-				ID: order.Coffee.ID,
-			},
-			Quantity: order.Quantity,
-		})
-	}
-
-	rb, err := json.Marshal(ois)
 	if err != nil {
 		return nil, err
 	}
