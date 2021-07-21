65692c3ba8072c01b582dc7fca3cd62c70db56b0
Tu Nguyen <im2nguyen@users.noreply.github.com>
2021-07-21 12:03:46 -0700

    Update models.go

--- models.go
+++ models.go
@@ -25,7 +25,7 @@ type Coffee struct {
 
 // Ingredient -
 type Ingredient struct {
-	ID       int    `json:"id"`
+	ID       int    `json:"ingredient_id"`
 	Name     string `json:"name"`
 	Quantity int    `json:"quantity"`
 	Unit     string `json:"unit"`
