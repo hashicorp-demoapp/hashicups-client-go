a83f7f62c4a5300591f24615517fea640a825549
Tu Nguyen <im2nguyen@gmail.com>
2022-02-10 21:20:18 -0800

    Update coffee/ing model

--- models.go
+++ models.go
@@ -14,13 +14,21 @@ type OrderItem struct {
 
 // Coffee -
 type Coffee struct {
-	ID          int          `json:"id"`
-	Name        string       `json:"name"`
-	Teaser      string       `json:"teaser"`
-	Description string       `json:"description"`
-	Price       float64      `json:"price"`
-	Image       string       `json:"image"`
-	Ingredient  []Ingredient `json:"ingredients"`
+	ID          int                `json:"id"`
+	Name        string             `json:"name"`
+	Teaser      string             `json:"teaser"`
+	Description string             `json:"description"`
+	Price       float64            `json:"price"`
+	Image       string             `json:"image"`
+	Ingredient  []CoffeeIngredient `json:"ingredients"`
+}
+
+// Ingredient -
+type CoffeeIngredient struct {
+	ID       int    `json:"ingredient_id"`
+	Name     string `json:"name"`
+	Quantity int    `json:"quantity"`
+	Unit     string `json:"unit"`
 }
 
 // Ingredient -
